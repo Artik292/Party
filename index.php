c66e6db49a4d88c76fb52d8ed32eb4e6f5e46378
Artur Konevnikov <artik292@gmail.com>
2018-04-07 20:21:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,26 +3,27 @@
 session_start();
 unset($_SESSION['admin_access']);
 
+echo $_ENV['pass'];
 require 'vendor/autoload.php';
 
 $app = new \atk4\ui\App('Запись на тусовку');
 $app->initLayout('Centered');
 
-require 'connecting.php';
+//require 'connecting.php';
 
-$form = $app->layout->add('Form');
+{$form = $app->layout->add('Form');
 $form->setModel(new Friend($db));
 $form->onSubmit(function($form) {
   $form->model->save();
   $form->success('Record updated');
   return new \atk4\ui\jsExpression('document.location = "index.php" ');
- });
+ });}
 
 $app->add(['ui'=>'divider']);
 
-$grid = $app->layout->add('Grid');
+{$grid = $app->layout->add('Grid');
 $grid->setModel(new Friend($db));
-$grid->addQuickSearch(['name','surname','phone_number','age']);
+$grid->addQuickSearch(['name','surname','phone_number','age']);}
 
 $app->add(['ui'=>'divider']);
 
