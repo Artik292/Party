6eae30238523bfa1bafbf541320d64b69a470d81
Artur Konevnikov <artik292@gmail.com>
2018-04-13 16:15:39 +0300

    Update check.php

--- check.php
+++ check.php
@@ -12,8 +12,9 @@ $check->addField('password',['type'=>'password','required'=>TRUE]);
 
 $form = $app->layout->add('Form');
 $form->setModel($check);
+$pass = $_ENV['pass'];
 $form->onSubmit(function($form) {
-  if ($form->model['password'] == 'password') {
+  if ($form->model['password'] == $pass) {
       $_SESSION['admin_access'] = 'fnupaw39r23rvwefk91248';
       return new \atk4\ui\jsExpression('document.location = "admin.php" ');
   } else {
